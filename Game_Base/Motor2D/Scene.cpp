a056cba45c3e514fe00d0f77c2097c9f1fc5f488
Ferran Martin Vilà <ferranmartinvila21@gmail.com>
2017-06-20 23:02:52 +0200

    Scene data management improved

--- Game_Base/Motor2D/Scene.cpp
+++ Game_Base/Motor2D/Scene.cpp
@@ -41,15 +41,42 @@ bool Scene::Enable()
 	floor_collider->body->SetType(b2BodyType::b2_staticBody);
 	floor_collider->body->GetFixtureList()->SetFriction(0.0f);
 
-	active = true;
-	enabled = true;
+	//Front Parallax --------
+	front_parallax = new Parallax(3);
+	front_parallax->SetTexture(App->tex->Load("scene/ground_texture.jpg"));
+	front_parallax->SetTextureRect({ 0,0,800,100 });
+	front_parallax->SetPosition(0, 900);
+
+	//Mid Parallax ----------
+	mid_parallax = new Parallax(5);
+	mid_parallax->SetTexture(App->tex->Load("scene/mid_texture.png"));
+	mid_parallax->SetTextureRect({ 0,0,400,450 });
+	mid_parallax->SetPosition(0, 450);
+
+	//Back Parallax ---------
+	back_parallax = new Parallax(2);
+	back_parallax->SetTexture(App->tex->Load("scene/skyline_texture.jpg"));
+	back_parallax->SetTextureRect({ 0,0,1600,900 });
+	back_parallax->SetPosition(0, 0);
+
+	//Play scene music
 	App->audio->PlayMusic(MUSIC_ID::MUSIC_IN_GAME);
+
+	active = enabled = true;
+
 	return true;
 }
 
 void Scene::Disable()
 {
+	//Release parallax system
+	RELEASE(front_parallax);
+	RELEASE(mid_parallax);
+	RELEASE(back_parallax);
+
+	//Release scene physic system
 	RELEASE(floor_collider);
+
 	enabled = active = false;
 }
 
@@ -68,23 +95,7 @@ bool Scene::Awake(pugi::xml_node& config)
 // Called before the first frame
 bool Scene::Start()
 {
-	//Front Parallax --------
-	front_parallax = new Parallax(3);
-	front_parallax->SetTexture(App->tex->Load("scene/ground_texture.jpg"));
-	front_parallax->SetTextureRect({ 0,0,800,100 });
-	front_parallax->SetPosition(0, 900);
-
-	//Mid Parallax ----------
-	mid_parallax = new Parallax(5);
-	mid_parallax->SetTexture(App->tex->Load("scene/mid_texture.png"));
-	mid_parallax->SetTextureRect({ 0,0,400,450 });
-	mid_parallax->SetPosition(0, 450);
-
-	//Back Parallax ---------
-	back_parallax = new Parallax(2);
-	back_parallax->SetTexture(App->tex->Load("scene/skyline_texture.jpg"));
-	back_parallax->SetTextureRect({ 0,0,1600,900 });
-	back_parallax->SetPosition(0, 0);
+	
 
 	return true;
 }
@@ -144,10 +155,12 @@ bool Scene::CleanUp()
 {
 	LOG("Freeing scene");
 	
-	delete front_parallax;
-	delete mid_parallax;
-	delete back_parallax;
+	if (front_parallax != nullptr) delete front_parallax;
+	if (mid_parallax != nullptr)delete mid_parallax;
+	if (back_parallax != nullptr)delete back_parallax;
 	
+	if (floor_collider != nullptr)delete floor_collider;
+
 	return true;
 }
 
